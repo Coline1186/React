9da4fec67052960c2a677050fadf74b2d48b2ef8
Coline1186 <coline.grosso@neuf.fr>
2024-03-30 10:36:30 +0100

    changement appel image

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,12 +3,11 @@ import PropTypes from "prop-types";
 function PokemonCard ({pokemon}){
   
   return (
-      <figure>
-          {pokemon.imgSrc ? <img></img> : <p>???</p>}
-          <img src = {pokemon.imgSrc}></img>
-          <figcaption><h1>{pokemon.name}</h1></figcaption>
-      </figure>
-      );
+    <figure>
+        {pokemon.imgSrc ? <img src = {pokemon.imgSrc}></img> : <p>???</p>}      
+        <figcaption><h1>{pokemon.name}</h1></figcaption>
+    </figure>
+    );
 }
 PokemonCard.propTypes = {
     pokemon : PropTypes.shape({
